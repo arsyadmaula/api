eef0c0545a348df083109619a5a758f9aaaa81c5
arsyadmaula <basrahilarsyad9@gmail.com>
2023-03-08 14:44:03 +0700

    feat:resto

---
+++ app/Http/Requests/StoreRestoRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Validation\Rule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRestoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => [
+                'required',
+                'string',
+                'max:250',
+                Rule::unique('restos','name'),
+            ],
+            'description' => [
+                'nullable',
+                'string',
+                'max:750',
+            ],
+            'address' => [
+                'required',
+                'string',
+                'max:750',
+            ],
+
+        ];
+    }
+}
+?>
--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,11 @@
 
 use App\Http\Controllers\AuthenticationController;
 use App\Http\Controllers\RestoController;
+use App\Http\Controllers\ReviewController;
 use Illuminate\Http\Request;
 use Illuminate\Routing\RouteRegistrar;
 use Illuminate\Support\Facades\Route;
+use Tests\Feature\AuthenticationTest;
 
 Route::post('/auth/login', [AuthenticationController::class,'login'])->name('auth.login');
 Route::post('/auth/register', [AuthenticationController::class,'register'])->name('auth.register');
@@ -13,6 +15,9 @@ Route::middleware('auth:sanctum')->group(function() {
 
     Route::get('/auth/profile', [AuthenticationController::class,'profile'])->name('auth.profile');
     Route::get('/auth/logout', [AuthenticationController::class,'logout'])->name('auth.logout');
+
+
+    Route::apiResource('reviews',ReviewsController::class);
 });
 
 Route::apiResource('restos',RestoController::class);
